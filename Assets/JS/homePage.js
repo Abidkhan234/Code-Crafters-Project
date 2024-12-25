5ba84caeaf8d80bdba52eb40cd6af3308b518450
Abidkhan234 <abidshahkhan237@gmail.com>
2024-12-25 15:27:29 +0500

    3-section complete

--- Assets/JS/homePage.js
+++ Assets/JS/homePage.js
@@ -15,3 +15,51 @@ navCross.addEventListener("click", () => {
 });
 
 // For Navbar
+
+// For Insperation-section 
+
+// Ins-section-slider
+
+const insImage = document.getElementById("ins-image");
+
+const insNext = document.getElementById("ins-next");
+
+const insPre = document.getElementById("ins-pre");
+
+let images = [
+    "Assets/Images/insperation-section/image-1.png",
+    "Assets/Images/insperation-section/image-2.png",
+    "Assets/Images/insperation-section/image-3.png"
+];
+
+let imageCounter = 0;
+
+insNext.addEventListener("click", () => {
+    imageCounter++;
+
+
+    if (imageCounter >= images.length) {
+        imageCounter = 0;
+    }
+
+
+    insImage.src = images[imageCounter];
+});
+
+
+insPre.addEventListener("click", () => {
+
+    if (imageCounter <= 0) {
+        imageCounter = images.length;
+    }
+
+    imageCounter--;
+
+
+    insImage.src = images[imageCounter];
+});
+
+// Ins-section-slider
+
+// For Insperation-section 
+
