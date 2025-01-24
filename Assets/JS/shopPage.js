188c695dcd514f6f040a510300e11c880305ef44
Abidkhan234 <abidshahkhan89@gmail.com>
2025-01-24 19:52:44 +0500

    Add to cart functionality done on shopPage and improve share-Section on homePage and added page loader to all complete page

--- Assets/JS/shopPage.js
+++ Assets/JS/shopPage.js
@@ -1,3 +1,13 @@
+// For pre-loader
+
+const preLoader = document.querySelector(".page-loader");
+
+window.addEventListener("load", () => {
+    preLoader.style.display = "none";
+})
+
+// For pre-loader
+
 // For Navbar
 
 const sideMenu = document.getElementById("side-nav-menu");
@@ -24,18 +34,19 @@ const cartCloseBtn = document.getElementById("cart-close-btn");
 
 const cartList = document.querySelector(".cart-list");
 
+const cartListOverlay = document.querySelector(".cart-list-overlay");
+
 cartOpenBtn.addEventListener("click", () => {
     cartList.classList.add("cart-list-open");
-    removingItem();
+    cartListOverlay.classList.add("cart-list-overlay-show");
 })
 
 cartCloseBtn.addEventListener("click", () => {
     cartList.classList.remove("cart-list-open");
+    cartListOverlay.classList.remove("cart-list-overlay-show");
 })
 
 // For cart open and close
-
-// For add to cart functionality
 
 // For pagination functionality
 
@@ -480,11 +491,11 @@ const paginationFunction = (num) => {
                 <img src="${item.image}" alt="...">
                 <span class="bg-danger position-absolute text-white top-0 end-0 p-2">${item.dis}</span>
               </div>
-              <div class="card-bottom ps-3 pt-0 d-flex gap-2 flex-column">
+              <div class="card-bottom p-3 d-flex gap-2 flex-column">
                 <h5 class="fw-semibold">${item.title}</h5>
                 <p>${item.subTitle}</p>
                 <div class="d-flex gap-3">
-                  <p style="font-size: large; font-weight: 600;" class="text-black fw-medium m-0">$ ${item.price}</p>
+                  <p style="font-size: large; font-weight: 600;" class="text-black fw-medium m-0 item-price">$ ${item.price}</p>
                   <s style="font-size: large; font-weight: 500;">${item.cutPrice}</s>
                 </div>
               </div>
@@ -547,6 +558,163 @@ const activeLink = (btn) => {
 
 // For active link functionnality
 
-// For add to cart functionality
+// For Add to cart function
+
+const addToCartBtns = document.querySelectorAll(".add-item");
+
+addToCartBtns.forEach((buttton) => {
+    buttton.addEventListener("click", (event) => {
+        let productCard = event.target.closest(".product-card")
+        addToCart(productCard);
+    })
+})
+
+const addToCart = (event) => {
+
+    const productTitle = event.querySelector(".card-bottom h5").innerText;
+    const productPrice = event.querySelector(".card-bottom .item-price").innerText;
+    const productImage = event.querySelector(".card-top img").src;
+
+    const cartContent = document.querySelector(".cart-content");
+
+    const cartItem = document.createElement("div");
+
+    cartItem.classList.add("item");
+
+    // For Duplicate Item Checking start
+
+    const cartItemTitle = cartContent.querySelectorAll(".item-title");
+
+    for (const itemTitle of cartItemTitle) {
+        if (itemTitle.innerText === productTitle) {
+            alert("This item already exist in cart.");
+            return;
+        }
+    }
+
+    // For Duplicate Item Checking end
+
+    cartItem.innerHTML = `
+
+                <div class="container-fluid overflow-hidden">
+                    <div class="row">
+                        <div class="col-sm-4 mb-3">
+                            <div class="item-image">
+                                <img src="${productImage}" alt="">
+                            </div>
+                        </div>
+
+                        <div class="col-sm-8">
+
+                            <div class="item-info d-flex gap-4 align-items-center h-100">
+
+                                <div class="d-flex flex-column gap-3">
+                                    <h4 class="item-title m-0 fw-semibold">${productTitle}</h4>
+                                    <div class="item-quantity-info d-flex justify-content-between align-items-center">
+                                        <input type="number" class="item-quantity" value="1">
+                                        <p class="item-price m-0 fs-6 fw-medium">${productPrice}</p>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <i class="fas fa-xmark" onclick = "removeItem(event)"></i>
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+    `;
+
+    cartContent.appendChild(cartItem);
+
+    // For quantity change Total price
+
+    cartItem.querySelector(".item-quantity").addEventListener("change", (event) => {
+        let quantity = event.target;
+
+        if (isNaN(quantity.value) || quantity.value <= 0) {
+            quantity.value = 1;
+        }
+
+        totalPriceUpdate();
+    })
+
+    // For quantity change Total price
 
 
+    totalPriceUpdate();
+
+}
+
+// For Add to cart function
+
+// For item remove 
+
+const removeItem = (item) => {
+
+    let clickedItem = item.target.closest(".item");
+
+    clickedItem.remove();
+
+    cartTotalItemCount();
+
+    totalPriceUpdate();
+
+
+}
+
+// For item remove 
+
+// For total Price update
+
+const totalPriceUpdate = () => {
+
+    const totalItem = document.querySelectorAll(".item");
+
+    const totalPriceElement = document.getElementById("totalPrice");
+
+    let total = 0;
+
+    totalItem.forEach((item) => {
+        const priceElement = item.querySelector(".item-price");
+        const quantityElement = item.querySelector(".item-quantity");
+        const price = parseFloat(priceElement.innerText.replace("$ ", ""));
+        const quantity = quantityElement.value;
+
+        total += price * quantity;
+    })
+
+    totalPriceElement.innerText = `$ ${total}`;
+
+}
+
+
+// For total Price update
+
+// For cart item count
+
+const addItemBtn = document.querySelectorAll(".add-item");
+let totalItem = 0;
+
+addItemBtn.forEach((button) => {
+    button.addEventListener("click", () => {
+        cartTotalItemCount();
+    })
+});
+
+const cartTotalItemCount = () => {
+
+    const cartItemCountElement = document.getElementById("cart-item-count");
+
+    const cartContent = document.querySelector(".cart-content");
+
+    totalItem = cartContent.children.length;
+
+    cartItemCountElement.innerText = totalItem;
+
+}
+
+// For cart item count
